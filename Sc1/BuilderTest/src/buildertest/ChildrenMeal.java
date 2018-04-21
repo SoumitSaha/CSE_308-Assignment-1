cb3016432d9bbb727fe4304a1cd3e458756ef368
SoumitSaha <1505047.sks@ugrad.cse.buet.ac.bd>
2018-04-21 23:06:04 +0600

    ChildrenMeal class implementing Crew(Builder) interface completed

--- Sc1/BuilderTest/src/buildertest/ChildrenMeal.java
+++ Sc1/BuilderTest/src/buildertest/ChildrenMeal.java
@@ -2,8 +2,42 @@ package buildertest;
 
 /**
  *
- * @author SOUMIT
+ * @author ASUS
  */
-public class ChildrenMeal {
+public class ChildrenMeal implements Crew{
+
+    private Product meal;
+    private String main;
+    private String side;
+    private String toy;
+    private String drinks;
+    
+    public ChildrenMeal(String MainDish, String SideDish, String Toy, String Drinks){
+        main = MainDish;
+        side = SideDish;
+        toy = Toy;
+        drinks = Drinks;
+        meal = new Product();
+    }
+    @Override
+    public void prepareMainDish() {
+        meal.addItemInBag(main);
+    }
+
+    @Override
+    public void prepareSideDish() {
+        meal.addItemInBag(side);
+    }
+
+    @Override
+    public void getToy() {
+        meal.addItemInBag(toy);
+    }
+    
+    @Override
+    public Product getChildrenMeal(){
+        meal.addItemInCup(drinks);
+        return meal;
+    }
     
 }
--- Sc1/BuilderTest/src/buildertest/Crew.java
+++ Sc1/BuilderTest/src/buildertest/Crew.java
@@ -9,5 +9,5 @@ public interface Crew {
     void prepareMainDish();
     void prepareSideDish();
     void getToy();
-    
+    public Product getChildrenMeal();
 }
