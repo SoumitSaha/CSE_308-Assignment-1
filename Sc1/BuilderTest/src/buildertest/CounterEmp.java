48d9a4c2be1b9bdaa86e126d3afc6e04ee1554d1
SoumitSaha <1505047.sks@ugrad.cse.buet.ac.bd>
2018-04-21 22:27:13 +0600

    Director (CounterEmp) created

---
+++ Sc1/BuilderTest/src/buildertest/CounterEmp.java
@@ -0,0 +1,15 @@
+package buildertest;
+
+/**
+ *
+ * @author SOUMIT
+ */
+public class CounterEmp {
+    Crew emp;
+    public void prepareChildrenMeal(Crew C){
+        emp  = C;
+        emp.prepareMainDish();
+        emp.prepareSideDish();
+        emp.getToy();
+    }
+}
