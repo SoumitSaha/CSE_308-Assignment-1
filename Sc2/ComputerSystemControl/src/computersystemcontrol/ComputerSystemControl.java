56f9eacbd1f83366a8b467708d3cb8f292ae8bbb
SoumitSaha <1505047.sks@ugrad.cse.buet.ac.bd>
2018-04-21 21:54:23 +0600

    Main Class for testing abstract factory pattern created

--- Sc2/ComputerSystemControl/src/computersystemcontrol/ComputerSystemControl.java
+++ Sc2/ComputerSystemControl/src/computersystemcontrol/ComputerSystemControl.java
@@ -1,12 +1,22 @@
 package computersystemcontrol;
 
+import java.util.Scanner;
+
 /**
  *
  * @author SOUMIT
  */
 public class ComputerSystemControl {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is the resolution(LOW, HIGH) : ");
+        String res;
+        res = in.nextLine();
+        ResolutionFactory fac = SystemController.getRsolutionFactory(res);
+        DispDriver dispdriver = fac.getDispDri(res);
+        PrintDriver printdriver = fac.getPrintDri(res);
+        dispdriver.dispDri();
+        printdriver.printDri();
     }
     
 }
